ac50982ac43c59d39ebec97cc74422edcbb49d9d
Moneymyck <anthom1253@gmail.com>
2026-05-24 16:28:08 +0200

    Delete admin/main_admin.py

--- admin/main_admin.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class GeostarAdminApp(App):
-    def build(self):
-        return Label(text="GEOSTAR Admin")
-
-if __name__ == "__main__":
-    GeostarAdminApp().run()
